e1763d87f0a08c611bcee22988941be9ad421eaa
Sam Quady <samquady1@gmail.com>
2021-02-13 14:28:01 -0800

    Added Client and Public Folders, Index HTML Page, Webpack Config, Build Script

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors - require('cors');
+
+app.use(cors());
+
+app.listen(port, () => {
+  console.log('server is now listening on port ' + port);
+})
