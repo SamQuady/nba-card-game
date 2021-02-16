98a8edce45e5f2225f85eb8677b8e5d2d5827d55
Sam Quady <samquady1@gmail.com>
2021-02-16 06:52:30 -0800

    Dumping, Creating, Copying Data to PSQL on Start Up

--- server/db/connection.js
+++ server/db/connection.js
@@ -11,6 +11,31 @@ const client = new Client({
 
 client.connect();
 
+const dumpTeamsTable = `
+DROP TABLE IF EXISTS teams;
+`;
+
+const dumpPlayerTable = `
+DROP TABLE IF EXISTS players;
+`;
+
+const dumpStatsTable = `
+DROP TABLE IF EXISTS stats;
+`;
+
+const createTeamsTable = `
+CREATE TABLE IF NOT EXISTS teams (
+  id int,
+  abbreviation varchar,
+  city varchar,
+  conference varchar,
+  division varchar,
+  full_name varchar,
+  name varchar,
+  PRIMARY KEY (id)
+)
+`;
+
 const createPlayerTable = `
 CREATE TABLE IF NOT EXISTS players (
   id int,
@@ -20,7 +45,11 @@ CREATE TABLE IF NOT EXISTS players (
   height_feet int,
   height_inches int,
   weight_pounds int,
-  PRIMARY KEY (id)
+  team int,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_player
+  FOREIGN KEY(team)
+    REFERENCES teams(id)
 );
 `;
 
@@ -28,7 +57,6 @@ const createStatsTable = `
 CREATE TABLE IF NOT EXISTS stats (
   games_played int,
   player_id int,
-  season int,
   min varchar,
   fgm real,
   fga real,
@@ -54,18 +82,105 @@ CREATE TABLE IF NOT EXISTS stats (
 );
 `;
 
-const createTeamsTable = `
-CREATE TABLE IF NOT EXISTS teams (
-  id int,
-  abbreviation varchar,
-  city varchar,
-  conference varchar,
-  division varchar,
-  full_name varchar,
-  name varchar,
-  PRIMARY KEY (id)
-)
+const copyTeams =  `
+COPY teams
+FROM '/Users/samquady/Work/mvp/nba-app/server/db/csv-data/team-records.csv'
+DELIMITER ','
+CSV HEADER;
 `;
+
+const copyPlayers = `
+COPY players
+FROM '/Users/samquady/Work/mvp/nba-app/server/db/csv-data/player-records.csv'
+DELIMITER ','
+CSV HEADER;
+`;
+
+const copyStats = `
+COPY stats
+FROM '/Users/samquady/Work/mvp/nba-app/server/db/csv-data/player-stats.csv'
+DELIMITER ','
+CSV HEADER;
+`;
+
+
+//-------Dropping Tables Queries-----------//
+
+client
+  .query(dumpStatsTable)
+  .then(res => {
+    console.log('dumped stats table')
+  })
+  .catch(err => {
+    console.log('could not dump stats table', err)
+  })
+
+client
+  .query(dumpPlayerTable)
+  .then(res => {
+    console.log('dumped players table')
+  })
+  .catch(err => {
+    console.log('could not dump players table', err)
+  })
+
+client
+  .query(dumpTeamsTable)
+  .then(res => {
+    console.log('dumped teams table')
+  })
+  .catch(err => {
+    console.log('could not dump teams table', err)
+  })
+
+//-------Creating Tables Queries-----------//
+
+client
+  .query(createTeamsTable)
+  .then(res => {
+    console.log('created teams table')
+  })
+  .catch(err => {console.log('could not create teams table', err)})
+client
+  .query(createPlayerTable)
+  .then(res => {
+    console.log('created players table')
+  })
+  .catch(err => {console.log('could not create players table', err)})
+client
+  .query(createStatsTable)
+  .then(res => {
+    console.log('created stats table')
+  })
+  .catch(err => {console.log('could not create stats table', err)})
+
+//-------Copying Data into Tables Queries-----------//
+
+client
+  .query(copyTeams)
+  .then(res => {
+    console.log('copied teams')
+  })
+  .catch(err => {
+    console.log('could not copy teams', err)
+  })
+client
+  .query(copyPlayers)
+  .then(res => {
+    console.log('copied players')
+  })
+  .catch(err => {
+    console.log('could not copy players', err)
+  })
+client
+  .query(copyStats)
+  .then(res => {
+    console.log('copied stats')
+  })
+  .catch(err => {
+    console.log('could not copy stats', err)
+  })
+  .then(() => client.end())
 
 module.exports = {
   client
