6b4235dbd3fa3869b1f8103a340ad2f7a543bbbc
Sam Quady <samquady1@gmail.com>
2021-02-17 08:21:54 -0800

    Added Styling to Cards When Clicked

--- client/components/card-view.jsx
+++ client/components/card-view.jsx
@@ -12,16 +12,50 @@ color: rgb(41, 41, 41);
 cursor: pointer;
 font-size: 20px;
 &:hover {box-shadow: inset 0 0 10px #000000;}
-`
+`;
+
+const ClickedCard = styled.div`
+background-color: rgb(230, 230, 230);
+length: 100px;
+border-radius: 4px;
+border-style: solid;
+border-color: rgb(104, 104, 104);
+border-width: 4px;
+color: rgb(41, 41, 41);
+cursor: pointer;
+font-size: 20px;
+&:hover {box-shadow: inset 0 0 10px #000000;}
+`;
 
 class CardView extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      clicked: false
+    };
   }
   render() {
-    return (
-      <Card onClick={(event) => this.props.onClick(this.props.info)}>
+    if (this.state.clicked) {
+      return (
+        <ClickedCard onClick={(event) => {
+          this.setState({clicked: [!this.state.clicked]})
+          this.props.onClick(this.props.info);
+        }}>
+          <div>{this.props.info[0][0].first_name + ' ' + this.props.info[0][0].last_name}</div>
+          <div>{this.props.info[0][0].height_feet + '\'' + this.props.info[0][0].height_inches + '"' + ' ' + this.props.info[0][0].weight_pounds + ' lbs. ' + this.props.info[0][0].position}</div>
+          <div>{this.props.info[0][2].full_name}</div>
+          <div>{this.props.info[0][1].min + 'mpg'}</div>
+          <div>{(Math.round(this.props.info[0][1].pts * 10) / 10) + 'pts'}</div>
+          <div>{(Math.round(this.props.info[0][1].reb * 10) / 10) + 'reb'}</div>
+          <div>{(Math.round(this.props.info[0][1].ast * 10) / 10) + 'ast'}</div>
+        </ClickedCard>
+      )
+    } else {
+      return (
+        <Card onClick={(event) => {
+          this.setState({clicked: [!this.state.clicked]})
+          this.props.onClick(this.props.info);
+        }}>
         <div>{this.props.info[0][0].first_name + ' ' + this.props.info[0][0].last_name}</div>
         <div>{this.props.info[0][0].height_feet + '\'' + this.props.info[0][0].height_inches + '"' + ' ' + this.props.info[0][0].weight_pounds + ' lbs. ' + this.props.info[0][0].position}</div>
         <div>{this.props.info[0][2].full_name}</div>
@@ -29,8 +63,9 @@ class CardView extends React.Component {
         <div>{(Math.round(this.props.info[0][1].pts * 10) / 10) + 'pts'}</div>
         <div>{(Math.round(this.props.info[0][1].reb * 10) / 10) + 'reb'}</div>
         <div>{(Math.round(this.props.info[0][1].ast * 10) / 10) + 'ast'}</div>
-      </Card>
-    )
+        </Card>
+      )
+    }
   }
 }
 
